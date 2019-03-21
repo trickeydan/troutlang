e55b4b2d1af1ecc96ad71bf01016bc8592fe1d7b
Dan Trickey <dan@trickey.io>
2019-03-21 16:22:42 +0000

    Extremely basic parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,17 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = someFunc
+import Data.Void
+import System.Environment
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Error
+
+type Parser = Parsec Void String
+
+main = do
+  input <- fmap head getArgs
+  parseTest singleLetterP input
+
+singleLetterP :: Parser Char
+singleLetterP = char 'h'
