e48e31c290a2677f2ae0bc60896fe985152ab5dc
Dan Trickey <dan@trickey.io>
2019-03-22 18:40:28 +0000

    Begin grammarification

--- src/Language/Trout/Grammar.hs
+++ src/Language/Trout/Grammar.hs
@@ -1,3 +1,37 @@
-module Language.Trout.Grammar (
+module Language.Trout.Grammar
+  (
+  ) where
 
-) where
+data InfixOperator
+  = AppendStream -- ++
+  | AppendFrame -- , 
+  | IntAdd -- +
+  | IntSubstract -- -
+  | IntDivide -- /
+  | IntMultiply -- *
+  | AssignmentOperator -- =
+  | Equals -- ==
+  | NotEquals -- !=
+  deriving (Show)
+
+data Identifier
+  = Variable String
+  | InputIndex Int
+  | OutputIndex Int
+
+data Expr =
+  Expr
+
+data Condition
+  = Equality Int
+             Int
+  | NotEqual Int
+             Int
+
+data Statement
+  = Iterator { iteratorIdentifier :: Identifier
+             , iteratorStatementList :: [Statement] }
+  | Assignment { assignmentIdentifier :: Identifier
+               , assignmentExpr :: Expr }
+  | ConditionalIf { condition :: Condition
+                  , statement :: Statement }
