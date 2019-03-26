5db6f511e6d5eb5a1cf27ca653b9bfdad46d48a4
Kajetan Champlewski <contact@kajetan.ch>
2019-03-26 12:47:33 +0000

    Add whitespace and comment parser

--- src/Language/Trout/Parser.hs
+++ src/Language/Trout/Parser.hs
@@ -5,7 +5,15 @@ module Language.Trout.Parser (
 ) where
 
 import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void
 import Data.Text
 
 type Parser = Parsec Void Text
+
+spaceConsumer :: Parser ()
+spaceConsumer = L.space
+  space1
+  (L.skipLineComment "//")
+  (L.skipBlockComment "/*" "*/")
