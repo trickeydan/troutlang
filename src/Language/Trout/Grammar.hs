768d43f2e44f430333df49d3740f724e22d96db2
Kajetan Champlewski <contact@kajetan.ch>
2019-03-27 13:13:37 +0000

    Derive Eq and Show for all Grammar

--- src/Language/Trout/Grammar.hs
+++ src/Language/Trout/Grammar.hs
@@ -3,10 +3,12 @@ module Language.Trout.Grammar where
 data StreamExpr = 
   Stream [FrameExpr]
   | AppendStream StreamExpr StreamExpr
+  deriving(Eq, Show)
 
 data FrameExpr =
   Frame [IntExpr]
   | AppendFrame FrameExpr FrameExpr
+  deriving(Eq, Show)
 
 data IntExpr =
   IntNum Int
@@ -14,19 +16,23 @@ data IntExpr =
   | IntSubtract IntExpr IntExpr
   | IntDivide IntExpr IntExpr
   | IntMultiply IntExpr IntExpr
+  deriving(Eq, Show)
 
 data Identifier =
   Variable String
   | InputIndex IntExpr
   | ReturnIndex IntExpr
+  deriving(Eq, Show)
 
 data Condition =
   Equals IntExpr IntExpr
   | NotEquals IntExpr IntExpr
+  deriving(Eq, Show)
 
 data Statement = 
   Iterator Identifier [Statement]
   | Assignment Identifier IntExpr
   | ConditionalIf Condition Statement
+  deriving(Eq, Show)
 
 type Program = [Statement]
