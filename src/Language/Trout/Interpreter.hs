8db364b72ed2c5bf14848a09d5a9fe470af9fe25
Dan Trickey <dan@trickey.io>
2019-03-27 18:42:36 +0000

    Added a state to the program

--- src/Language/Trout/Interpreter.hs
+++ src/Language/Trout/Interpreter.hs
@@ -1,23 +1,31 @@
 module Language.Trout.Interpreter where
 
+import Control.Monad.State
+
 import Language.Trout.Interpreter.Reduction
+import Language.Trout.Interpreter.State
 import Language.Trout.Grammar
 
-evalProgram :: Program -> IO ()
-evalProgram [] = putStrLn "Execution complete."
+runProgram :: Program -> TroutState ()
+runProgram program = do
+    evalProgram program
+    return ()
+
+evalProgram :: Program -> TroutState ()
+evalProgram [] = liftIO $ putStrLn "Execution complete."
 evalProgram (x:xs) = do 
     evalStatement x
     evalProgram xs
 
-evalStatement :: Statement -> IO ()
+evalStatement :: Statement -> TroutState ()
 evalStatement (PrintExpr intExpr) = evalPrintExpr intExpr
 evalStatement (PrintIdentifier ident) = evalPrintIdentifier ident
-evalStatement _ = putStrLn "Unimplemented Statement."
+evalStatement _ = liftIO $ putStrLn "Unimplemented Statement."
 
--- Print Statements
+-- -- Print Statements
 
-evalPrintExpr :: IntExpr -> IO ()
-evalPrintExpr expr = putStrLn $ show $ reduceIntExpr expr
+evalPrintExpr :: IntExpr -> TroutState ()
+evalPrintExpr expr = liftIO $ print $ reduceIntExpr expr
 
-evalPrintIdentifier :: Identifier -> IO ()
-evalPrintIdentifier ident = putStrLn $ show $ reduceIdentifier ident
+evalPrintIdentifier :: Identifier -> TroutState ()
+evalPrintIdentifier ident = liftIO $ print $ reduceIdentifier ident
