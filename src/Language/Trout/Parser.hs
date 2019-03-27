7983d6bdf2f84f55771b5fc89d5b274cde512862
Kajetan Champlewski <contact@kajetan.ch>
2019-03-27 15:59:33 +0000

    Add identifier and partial intExpr parser

--- src/Language/Trout/Parser.hs
+++ src/Language/Trout/Parser.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Language.Trout.Parser (
-
+  identifier,
+  intExpr
 ) where
 
 import Text.Megaparsec
@@ -9,6 +10,8 @@ import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void
 import Data.Text
+
+import Language.Trout.Grammar
 
 type Parser = Parsec Void Text
 
@@ -20,3 +23,22 @@ spaceConsumer = L.space
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme spaceConsumer
+
+symbol :: Text -> Parser Text
+symbol = L.symbol spaceConsumer
+
+identifier :: Parser Identifier
+identifier = lexeme $ (
+  variable <|>
+  inputIndex <|>
+  returnIndex )
+  where
+    variable :: Parser Identifier
+    variable = Variable <$> ((:) <$> letterChar <*> many alphaNumChar <?> "variable")
+    inputIndex :: Parser Identifier
+    inputIndex = InputIndex <$> between (symbol "[") (symbol "]") (intExpr <?> "input stream index")
+    returnIndex :: Parser Identifier
+    returnIndex = ReturnIndex <$> between (symbol "<") (symbol ">") (intExpr <?> "output stream index")
+
+intExpr :: Parser IntExpr
+intExpr = IntNum <$> (lexeme $ L.decimal)
