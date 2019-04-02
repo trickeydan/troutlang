2f5ac7b3aafd36be44843e8643bcb82f00149abb
Dan Trickey <dan@trickey.io>
2019-04-02 17:08:26 +0100

    Stream Variables

--- src/Language/Trout/Interpreter/Type/Stream.hs
+++ src/Language/Trout/Interpreter/Type/Stream.hs
@@ -25,9 +25,9 @@ evalStreamExpr (AppendStream expr1 expr2) = do
     st1 <- evalStreamExpr expr1
     st2 <- evalStreamExpr expr2
     return $ st1 ++ st2
-evalStreamExpr (StreamIdentifier _) =  do
-    notImplemented "StreamIdentifiers are not implemented."
-    return []
+evalStreamExpr (StreamIdentifier ident) =  do
+    expr <- evalStreamIdentifier ident
+    evalStreamExpr expr
 evalStreamExpr (Iterator _ _) = do
     notImplemented "Iterators are not implemented"
     return []
